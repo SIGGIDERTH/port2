6c0a450eb13c9c01d66b02ade31232dbf3ef5452
SIGGIDERTH <130973100+SIGGIDERTH@users.noreply.github.com>
2023-04-19 14:39:47 +0900

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,19 @@
+/*
+$(function(){
+    $(".con").on("mouseenter",function(){
+        vid=$(this).find("video").get(0);
+        vid.play();
+        $(this).stop().animate({width:"45%"});
+        $(this).children("video").stop().animate({opacity:"0.5"});
+        $(this).children("div").stop().animate({left:"0%",opacity:"1"});
+    });
+
+
+    $(".con").on("mouseleave",function(){
+        vid.pause();
+        $(this).stop().animate({width:"25%"});
+        $(this).children("video").stop().animate({opacity:"0"});
+        $(this).children("div").stop().animate({left:"-130%",opacity:"0"});
+    });
+});
+*/
